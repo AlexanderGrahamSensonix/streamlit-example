1ea4e9fe98eda1aca479b07d3a7151f906e9ff3f
AlexanderGrahamSensonix <89032479+AlexanderGrahamSensonix@users.noreply.github.com>
2021-09-08 13:40:42 -0500

    first_app demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,82 @@
-import numpy as np
 import streamlit as st
-import plotly.express as px
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import time
+#st.title('My first app')
+
+#st.write("Here's our first attempt at using data to create a table:")
+#st.write(pd.DataFrame({
+#    'first column': [1, 2, 3, 4],
+#    'second column': [10, 20, 30, 40]
+#}))
+
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+#option = st.selectbox(
+#    'Which number do you like best?',
+#     df['first column'])
+#
+#'You selected: ', option
 
 
 
-fileName = "20210826Z111434.txt"
-totalTimeInSeconds = 0.8446
-numberOfDataPoints = 4224
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.sidebar.line_chart(chart_data)
+
+left_column, right_column = st.columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+  right_column.write("Woohoo!")
+
+'Starting a long computation...' 
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
+
+expander = st.expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")
 
 
-xData = np.linspace(0, totalTimeInSeconds, numberOfDataPoints)
-yData = np.loadtxt(fileName, delimiter=",")
+option = st.sidebar.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected:', option
 
 
 
 
-fig = px.line(x=xData, y=yData)
-st.plotly_chart(fig)
+
+
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
